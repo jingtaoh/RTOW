c1620401021667e99cfdf5f4ce410159580ced3c
Jingtao Huang <jingtaoh@usc.edu>
2021-05-18 17:57:46 -0700

    2.3 Adding a Progress Indicator

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines reainning: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double (i) / (image_width - 1);
             auto g = double (j) / (image_height - 1);
@@ -25,6 +26,6 @@ int main() {
         }
     }
 
-    std::cout << "Hello, World!" << std::endl;
+    std::cerr << "\nDone.\n";
     return 0;
 }
