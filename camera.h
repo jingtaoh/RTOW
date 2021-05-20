f3caee867e42380aa6ee8bfeee5af4daa1841041
Jingtao Huang <jingtaoh@usc.edu>
2021-05-20 15:54:57 -0700

    12.2 Generating Sample Rays

--- camera.h
+++ camera.h
@@ -32,7 +32,13 @@ public:
     }
 
     ray get_ray(double s, double t) const {
-        return ray(origin, lower_left_corner + s * horizontal + t * vertical - origin);
+        vec3 rd = lens_radius * random_in_unit_disk();
+        vec3 offset = u * rd.x() + v * rd.y();
+
+        return ray(
+                origin + offset,
+                lower_left_corner + s * horizontal + t * vertical - origin - offset
+               );
     }
 
 private:
